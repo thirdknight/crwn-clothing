dd11f8d1d6ce1bc43eb4fa130d70de781b75f9bc
Edinson <edinson.ningles@gmail.com>
2023-10-22 00:27:19 +0200

    Add React Router Link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 
-const Navigation = () => {
-  return(
-    <div>
-      <div>
-        <h1>I'm the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-}
 
 const Shop = () =>{
   return <h1>I'm the shop page</h1>
